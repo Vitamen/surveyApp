dc80f50d64ad10dfeb05883fb2e90d1d9e6d048f
Sophie Zhou <soph.zhou@gmail.com>
2012-02-10 17:26:42 -0500

    first play commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,7 +10,22 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
+    	Topic topic1 = fetchTopic();
+    	Topic topic2 = fetchTopic();
+    	
+    	renderArgs.put("topic1", topic1);
+    	renderArgs.put("topic2", topic2);
         render();
+    }
+    
+    public static void fetchTopics() {
+    	Topic topic1 = fetchTopic();
+    	Topic topic2 = fetchTopic();
+    }
+    
+    public static Topic fetchTopic() {
+    	Topic topic = null;
+    	return topic;
     }
 
 }
