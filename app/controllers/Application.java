d2237cd6f797cf8faf39f37d2fb299ece263e731
rohitsalim <rohitsalim@gmail.com>
2012-02-19 22:59:37 -0500

    Facebook-authentication enabled
    Please install the fbgraph API before running app

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,23 @@
 package controllers;
 
 import play.*;
+import play.modules.facebook.FbGraph;
+import play.modules.facebook.FbGraphException;
+import play.modules.facebook.Parameter;
 import play.mvc.*;
+import play.mvc.Scope.Session;
 
 import java.util.*;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import models.*;
 
 public class Application extends Controller {
 
+	
+	
     public static void index() {
     	RSSEngine.fetchNews();
     	System.out.println("done fetching news");
@@ -19,6 +28,40 @@ public class Application extends Controller {
     	renderArgs.put("topic2", topic2);
         render();
     }
+    
+    public static void displayFriends(){
+    	render();
+    }
+    
+    public static void facebookLogin() {
+        try {
+            JsonObject profile = FbGraph.getObject("me"); // fetch the logged in user
+            String email = new String("default");
+            if (profile != null)
+            	email = profile.get("email").getAsString(); // retrieve the email
+            System.out.println(email);
+                      
+            // do useful things
+            Session.current().put("username", email); // put the email into the session (for the Secure module)
+            
+            System.out.println(Session.current().getAuthenticityToken().toString());
+        } catch (FbGraphException fbge) {
+            flash.error(fbge.getMessage());
+            if (fbge.getType() != null && fbge.getType().equals("OAuthException")) {
+                Session.current().remove("username");
+            }
+        }
+        displayFriends();
+    }
+
+    public static void facebookLogout() {
+    	System.out.println("Logging out");
+        Session.current().remove("username");
+        FbGraph.destroySession();
+        index();
+    }
+
+    
 
     public static void fetchTopics() {
     	Topic topic1 = fetchTopic();
